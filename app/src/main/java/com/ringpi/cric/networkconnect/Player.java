a77f57c3ec1c8854271d8135ade068f6b0198f87
ringpisolve <ringpisolve@gmail.com>
2020-08-23 17:28:53 -0500

    Updated Player obj. Added Player Operations utility class with method signatures only.

--- app/src/main/java/com/ringpi/cric/networkconnect/Player.java
+++ app/src/main/java/com/ringpi/cric/networkconnect/Player.java
@@ -4,8 +4,27 @@ public class Player {
 
     private int pid;
     private String name;
+
+    public boolean isFreeAgent() {
+        return isFreeAgent;
+    }
+
+    public void setFreeAgent(boolean freeAgent) {
+        isFreeAgent = freeAgent;
+    }
+
     private boolean isFreeAgent;
     private String franchiseName;
+
+    public boolean isInPlayingEleven() {
+        return isInPlayingEleven;
+    }
+
+    public void setInPlayingEleven(boolean inPlayingEleven) {
+        isInPlayingEleven = inPlayingEleven;
+    }
+
+    private boolean isInPlayingEleven;
 
     public Player(int pid, String name, boolean isFreeAgent, String franchiseName) {
         this.pid = pid;
---
+++ app/src/main/java/com/ringpi/cric/networkconnect/PlayerOperations.java
@@ -0,0 +1,22 @@
+package com.ringpi.cric.networkconnect;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class PlayerOperations {
+
+    public void addToPlayingEleven(Player player, String teamName, FirebaseFirestore fireDatabase) {
+
+    }
+
+    public void removeFromPlayingEleven(Player player, String teamName, FirebaseFirestore fireDatabase) {
+
+    }
+
+    public void addToSquad(Player player, String teamName, FirebaseFirestore firebaseFirestore) {
+
+    }
+
+    public void removeFromSquad(Player player, String teamName, FirebaseFirestore firebaseFirestore) {
+
+    }
+}
