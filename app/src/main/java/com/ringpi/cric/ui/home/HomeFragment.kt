0cbe803b23387df860e1b6b2f24f1b7c3467945b
ringpisolve <ringpisolve@gmail.com>
2020-08-26 22:53:30 -0500

    Added simple list view to Home fragment

--- app/src/main/java/com/ringpi/cric/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ringpi/cric/ui/home/HomeFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.ringpi.cric.MainActivity
 import com.ringpi.cric.R
 
 
@@ -27,5 +30,18 @@ class HomeFragment : Fragment() {
 
         return view
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val context = context as MainActivity
+        val users = arrayOf(
+            "Virat Kohli", "Rohit Sharma", "Steve Smith",
+            "Kane Williamson", "Ross Taylor"
+        )
+        val lv = context.findViewById(R.id.study_level_list) as ListView
+        val adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, users)
+        lv.adapter = adapter
     }
 }
