1105dda6610cd2edce891c3c98cade0398eef5ea
TarunReddy099 <tarunreddy099@gmail.com>
2020-08-27 22:09:50 +0530

    bowling obj modified

--- app/src/main/java/com/ringpi/cric/networkconnect/BowlingScore.java
+++ app/src/main/java/com/ringpi/cric/networkconnect/BowlingScore.java
@@ -1,25 +1,17 @@
 package com.ringpi.cric.networkconnect;
 
-public class BowlingScore {
-    public float overs=0;
+    public class BowlingScore {
+
     public int maidens=0;
-    public int runs = 0;
     public int wickets;
     public float economy=0;
-    public int dots=0;
-    public int noOfFours = 0;
-    public int noOfSixes;
-
-    public BowlingScore( float overs,int maidens,int runs,int wickets,float economy, int dots, int noOfFours, int noOfSixes) {
 
 
-        this.overs = overs;
-        this.maidens = maidens;
-        this.runs = runs;
+    public BowlingScore( int maidens,int wickets,float economy) {
+
+
         this.wickets = wickets;
         this.economy = economy;
-        this.dots = dots;
-        this.noOfFours = noOfFours;
-        this.noOfSixes = noOfSixes;
+
     }
 }
