b090a1993bd1bab15a714947394b744fc7b7164f
TarunReddy099 <tarunreddy099@gmail.com>
2020-09-05 23:17:49 +0530

    new fragment added

---
+++ app/src/main/java/com/ringpi/cric/squadpointscalculation/MyListAdapter1.kt
@@ -0,0 +1,36 @@
+package com.ringpi.cric.squadpointscalculation
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.ringpi.cric.R
+
+
+class MyListAdapter1(var mCtx: Context, var resource:Int, var items:List<Model1>)
+    : ArrayAdapter<Model1>( mCtx , resource , items ){
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val layoutInflater : LayoutInflater = LayoutInflater.from(mCtx)
+
+        val view : View = layoutInflater.inflate(resource , null )
+        val imageView : ImageView = view.findViewById(R.id.iconIv1)
+        var textView : TextView = view.findViewById(R.id.titleTv1)
+        var textView1 : TextView = view.findViewById(R.id.descTv1)
+
+
+        var person : Model1 = items[position]
+
+        imageView.setImageDrawable(mCtx.resources.getDrawable(person.photo))
+        textView.text = person.title
+        textView1.text = person.desc
+
+
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/ringpi/cric/squadpointscalculation/SquadpointsFragment.kt
@@ -0,0 +1,46 @@
+package com.ringpi.cric.squadpointscalculation
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ListView
+import com.google.firebase.firestore.FirebaseFirestore
+import com.ringpi.cric.R
+import com.ringpi.cric.ui.home.Model
+import com.ringpi.cric.ui.home.MyListAdapter
+
+class SquadpointsFragment : Fragment() {
+
+    lateinit var listView : ListView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.squadpoints_fragment, container, false)
+        val view: View = inflater.inflate(R.layout.squadpoints_fragment, container, false)
+
+        val mFirestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+
+        listView = view.findViewById(R.id.study_level_list1)
+        var list = mutableListOf<Model1>()
+
+        list.add(Model1("Title 1",   "Description One...",   R.mipmap.ic_launcher  ))
+        list.add(Model1("Title Two",   "Description Two...",   R.mipmap.ic_launcher_round  ))
+        list.add(Model1("Title Three", "Description Three...", R.mipmap.ic_launcher  ))
+        list.add(Model1("Title Four",  "Description Four...",  R.mipmap.ic_launcher_round  ))
+        list.add(Model1("Title Five",  "Description Five...",  R.mipmap.ic_launcher  ))
+
+        listView.adapter = context?.let { MyListAdapter1(it,R.layout.row2,list) }
+
+
+        return view
+        return root
+    }
+
+
+}
